26eb60ee7f1740605dcec767d09df3733869bd72
ykknai <luis.saavedra0800@gmail.com>
2025-10-15 17:48:47 -0700

    :task remove all

--- API_AGENDAMIENTO/src/middlewares/verificarToken.js
+++
@@ -1,9 +0,0 @@
-export default function verificarToken(req, res, next) {
-    const token = req.headers['x-admin-create-token'];
-
-    if (!token || token !== process.env.ADMIN_CREATION_TOKEN) {
-        return res.status(403).json({ message: "Token de creación inválido o ausente" });
-    }
-
-    next();
-}
--- cliente_react/barbie_lashart_react/src/components/RutaPrivada.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function RutaPrivada({ children, rolNecesario }) {
-    const rol = localStorage.getItem("rol");
-    if (!rol) return <Navigate to="/login" />; // no logueado
-    if (rolNecesario && rol !== rolNecesario) return <Navigate to="/" />; // rol no permitido
-    return children;
-}
